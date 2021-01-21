7be3ef1f225e516db9eca86da9f88a8dbf3c2a1a
Drew Thompson <drewallynthompson@gmail.com>
2021-01-21 01:35:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,10 +18,9 @@ function BoardMember(name, homeState, training){
   this.releasePressStatement = () => {
     return 'You will see great things from Scuber.';
   }
-}
-
-BoardMember.prototype.sayHi = () => {
-  console.log(this.name);
+  
+  this.sayHi = () => {
   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+}
 }
 
